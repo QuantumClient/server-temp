d56b7bfe02f86dc3d2cf5cda828645f24ea212e1
ChiquitaV2 <74295674+ChiquitaV2@users.noreply.github.com>
2021-07-30 14:09:26 -0500

    golang

--- services/backend/util/utils.go
+++ services/backend/util/utils.go
@@ -2,9 +2,12 @@ package util
 
 import "regexp"
 
-
 func IsUUID(uuid string) bool {
 	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
 	return r.MatchString(uuid)
 }
 
+func Alphanumeric3p(s string) bool {
+	r := regexp.MustCompile(`^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){3,50}[a-zA-Z0-9]$`)
+	return r.MatchString(s)
+}
